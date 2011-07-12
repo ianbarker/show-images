0a29fb16bd2d6bd3e89da7f75120d803c7703a97
Ian Barker <me@ian-barker.co.uk>
2011-07-12 12:53:12 +0100

    improved flickr photo_id regex

--- jquery.show-images.js
+++ jquery.show-images.js
@@ -3,16 +3,16 @@
  * 
  * CONFIG:
  * 
- * change the value of imageWidth if you want the images to display larger or smaller
+ * change the value of imageWidth if you want the images to display larger or
+ * smaller
  * 
  * USAGE:
  * 
- * 1. copy the code below into clipboard (cmd+c or ctrl+c)
- * 2. add a bookmark to your browser
- * 3. edit the bookmarks url and delete what's in there
- * 4. type in 'javascript: ' (without the quotes)
- * 5. paste in the code (cmd+v or ctrl+v)
- * 6. go to reddit... find a page with images you want to view and click on the bookmark
+ * 1. copy the code below into clipboard (cmd+c or ctrl+c) 2. add a bookmark to
+ * your browser 3. edit the bookmarks url and delete what's in there 4. type in
+ * 'javascript: ' (without the quotes) 5. paste in the code (cmd+v or ctrl+v) 6.
+ * go to reddit... find a page with images you want to view and click on the
+ * bookmark
  * 
  */
 
@@ -20,6 +20,25 @@ var loaded = [];
 var imageWidth = '400px';
 var apiKey = '3c5ee387435906f7b1fd1534732b8e7f';
 var flickrUrl = 'http://api.flickr.com/services/rest/';
+
+var getFlickrImage = function(href) {
+	var photoId = href.match(/\/([0-9]+)\//)[0].replace('/','');
+	$.getJSON(flickrUrl, {
+		method : 'flickr.photos.getSizes',
+		api_key : apiKey,
+		photo_id : photoId,
+		format : 'json',
+		nojsoncallback : 1
+	}, function(data) {
+		if (data.stat == 'ok') {
+			return data.sizes.size[3].source;
+		} else {
+			console.debug('ERROR: Failed to get flickr image from (' + href + ') ' + photoId);
+			return false;
+		}
+	});
+};
+
 $('.content').find('a').each(function() {
 	var $this = $(this);
 	var href = $this.attr('href');
@@ -35,30 +54,21 @@ $('.content').find('a').each(function() {
 				});
 				$(this).closest('div.thing').after(img);
 			} else {
-				if ( href.indexOf('flickr') >= 0 ) {
-					var photoId = href.match(/([0-9]+)/)[0];
-					$.getJSON(flickrUrl,{
-						method: 'flickr.photos.getSizes',
-						api_key: apiKey,
-						photo_id: photoId,
-						format: 'json',
-						nojsoncallback: 1
-					},function(data) {
-						if ( data.stat == 'ok') {
-							href = data.sizes.size[3].source;
-							var img = $('<img />').attr('src', href).css({
-								width : imageWidth,
-								margin : '8px 0 8px 74px'
-							});
-							$this.closest('div.thing').after(img);
-						}
-					});
+				if (href.indexOf('flickr') >= 0) {
+					href = getFlickrImage(href);
+					if (href) {
+						var img = $('<img />').attr('src', href).css({
+							width : imageWidth,
+							margin : '8px 0 8px 74px'
+						});
+						$this.closest('div.thing').after(img);
+					}
 				}
 			}
 		}
 	}
 });
-$('div.usertext-body').find('a').each( function() { 
+$('div.usertext-body').find('a').each(function() {
 	var href = $(this).attr('href');
 	if ($.inArray(loaded, href) < 0) {
 		loaded.push(href);
@@ -67,28 +77,19 @@ $('div.usertext-body').find('a').each( function() {
 				href += '.jpg';
 			var img = $('<img />').attr('src', href).css({
 				width : imageWidth,
-				display: 'inline'
+				display : 'inline'
 			});
 			$(this).after(img);
 		} else {
-			if ( href.indexOf('flickr') >= 0 ) {
-				var photoId = href.match(/([0-9]+)/)[0];
-				$.getJSON(flickrUrl,{
-					method: 'flickr.photos.getSizes',
-					api_key: apiKey,
-					photo_id: photoId,
-					format: 'json',
-					nojsoncallback: 1
-				},function(data) {
-					if ( data.stat == 'ok') {
-						href = data.sizes.size[3].source;
-						var img = $('<img />').attr('src', href).css({
-							width : imageWidth,
-							display: 'inline'
-						});
-						$this.closest('div.thing').after(img);
-					}
-				});
+			if (href.indexOf('flickr') >= 0) {
+				href = getFlickrImage(href);
+				if (href) {
+					var img = $('<img />').attr('src', href).css({
+						width : imageWidth,
+						display : 'inline'
+					});
+					$this.closest('div.thing').after(img);
+				} 
 			}
 		}
 	}
